3515ed92bb86e6174242d9f5a2f78702f41cefa7
Vishnuprasad <viktor.prasad@gmail.com>
2024-10-20 23:04:14 +0100

    fix failing build

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -16,20 +16,12 @@ const config: GatsbyConfig = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "static/images/icon.png",
       },
     },
     "gatsby-plugin-mdx",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
     {
       resolve: "gatsby-source-filesystem",
       options: {
